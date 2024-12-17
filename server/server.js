4ca9aa835675705798cac16792a91ac166324742
Hendrick <retshephilengm@gmail.com>
2024-12-17 19:18:24 +0200

    update

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use(session({
     resave: false, 
     saveUninitialized: false, 
     cookie: { 
-        secure: false, 
+        secure: true, 
         maxAge: 1000 * 60 * 60 * 24 * 3,
         sameSite: 'None'
     }
