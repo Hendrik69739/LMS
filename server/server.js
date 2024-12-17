35b566801f424cfab7277473d0079a068f16bdeb
Hendrick <retshephilengm@gmail.com>
2024-12-17 15:09:26 +0200

    update

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use(session({
     resave: false, 
     saveUninitialized: false, 
     cookie: { 
-        secure: false, 
+        secure: true, 
         maxAge: 1000 * 60 * 60 * 24 * 3, 
         domain: '4.tcp.eu.ngrok.io'
     }
