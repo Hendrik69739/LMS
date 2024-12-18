a04c2955a6065998d19ff5b62934a97901736199
Hendrick <retshephilengm@gmail.com>
2024-12-18 12:46:08 +0200

    update

--- server/Auth/authentication.js
+++ server/Auth/authentication.js
@@ -28,7 +28,7 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
     const { password, email } = req.body;
-
+    console.log('req reached authentication site')
     try {
         const { rows } = await db.query('SELECT * FROM students WHERE email = ? AND password = ?', [email, password]);
         
