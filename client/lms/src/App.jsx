d3f547910e633532c8a0641a8d5dbaaa9cc19dee
Hendrik <retshephilengm@gmail.com>
2024-12-19 20:29:19 +0200

    update

--- client/lms/src/App.jsx
+++ client/lms/src/App.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Navigate } from 'react-router-dom';
 import Login from './login';
 import Signup from './Signup';
 import Profile from './Profile';
 
 
+
 function App() {
   return (
     <Router>
-      <Routes>
+      <Switch>
         <Route path='/login' element={<Login />} />
         <Route path='/' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/profile/*' element={<Profile />} /> 
         <Route path='*' element={<Navigate to='/login' />} />
-      </Routes>
+      </Switch>
     </Router>
   );
 }
 
 export default App;
+
+
+
+
+
