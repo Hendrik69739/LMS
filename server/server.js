41ae2e851ddd46350f32e7d9cdff8500a249bd85
Hendrik <retshephilengm@gmail.com>
2024-12-27 21:22:07 +0200

    update

--- server/server.js
+++ server/server.js
@@ -139,7 +139,7 @@ app.post('/assignments', async (req, res) => {
 
 app.post('/user-info', async (req, res) => {
     try {
-        const [rows] = await pool.query('SELECT * FROM students.students WHERE email = $1', [req.body.user]);
+        const rows = await pool.query('SELECT * FROM students.students WHERE email = $1', [req.body.user]);
         console.log('Query Result:', rows);
         
         if (!Array.isArray(rows)) {
