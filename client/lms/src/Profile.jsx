74064b3abb638fdf9cc1ebc1967fd3c8b79e477a
Hendrik <retshephilengm@gmail.com>
2024-12-31 11:46:47 +0200

    update

--- client/lms/src/Profile.jsx
+++ client/lms/src/Profile.jsx
@@ -4,7 +4,7 @@ import Content from './content';
 import Admin from './admin';
 
 function Profile() {
-   /* const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [user, setUser] = useState('');
     const navigate = useNavigate();
 
@@ -32,10 +32,10 @@ function Profile() {
 
         checkSession();
     }, [navigate]);
-*/
+
     return (
     
-      /*  <>
+        <>
       {isAuthenticated ? (
             user === 'admin@gmail.com' ? (
                 <Admin />
@@ -45,8 +45,7 @@ function Profile() {
         ) : (
             <p>You are not logged in, please return to the login page.</p>
         )} 
-    </> */
-    <Content/>
+    </> 
     );
 }
 
